df189a6160c5995ea56f2b7d78f6c1f56ffba9b1
Andrew T. Baker <abaker@twilio.com>
2015-06-02 20:12:02 -0700

    Adding hold route

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # leviathan
 A scary sounding service that helps screen incoming phone calls from unrecognized numbers
+
+Try it yourself by calling (202) 759-0452.
+
+## Notes
+
+Start first hold music at ~30s
+Start second hold music at ~60s
--- leviathan.py
+++ leviathan.py
@@ -1,16 +1,43 @@
-from flask import Flask
+from flask import Flask, request, redirect, send_from_directory
 import twilio.twiml
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_monkey():
+def get_name():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+
+    # Send the initial query
+    intro = "Hello, you have reached Mr. Baker's office. Who may I say is calling?"
+    resp.say(intro)#  voice='Alice', language='en-GB')
+
+    # Record the caller's response
+    resp.record(maxlength=5, action='/hold', transcribe=True, transcribe_callback='/name')
 
     return str(resp)
+
+@app.route('/hold', methods=['GET', 'POST'])
+def hold():
+	"""Keeps users on hold indefinitely"""
+	resp = twilio.twiml.Response()
+	resp.play('/girl-from-ipanema', loop=0)
+
+	return str(resp)
+
+@app.route('/girl-from-ipanema')
+def hold_music():
+	"""Soothing hold music / some versions of Hell"""
+	return send_from_directory('', 'girl_from_ipanema.mp3')
+
+@app.route('/name', methods=['GET', 'POST'])
+def review_name():
+	"""Forwards a caller's name to the user"""
+	resp = twilio.twiml.Response()
+
+	import pdb; pdb.set_trace()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
